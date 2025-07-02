ff913cdc308e5ba50eec5c35681d6cca1db03473
HyoJunBang <qkdgywns1004@naver.com>
2025-07-02 15:08:47 +0900

    <react> 변수/상수 선언

---
+++ section01/chapter04.js
@@ -0,0 +1,21 @@
+//변수 (let, var, const)
+let num0 = "10";
+let num1 = "bhj";
+let num2 = true;
+let num3 = null;
+console.log(num0);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+//변수 선언은 동적 타입이다. => 상수에 의해 타입이 결정 된다.
+
+//상수
+const birth = "1997.02.08";
+console.log(birth);
+
+// 상수는 선언과 동시에 집어 넣어줘야 한다.
+/* 에러 뜸!!
+const birth;
+birth = '1997'; */
+
+//var는 변수명이 같아도 여러번 선언 가능하니까.. 쓰지않는 것이 좋다.
