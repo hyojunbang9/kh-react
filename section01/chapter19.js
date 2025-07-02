07dbefba7d0e6842a0053df84ae79787a5b14081
HyoJunBang <qkdgywns1004@naver.com>
2025-07-02 17:14:31 +0900

    <react> indexOf,find,filter,map

--- section01/chapter19.js
+++ section01/chapter19.js
@@ -32,3 +32,39 @@ arr.forEach((e) => {
 });
 
 console.log(`4값 체크 유무 : ${arr.includes(4)}`);
+
+//배열 내장 객체 indexof
+arr.indexOf(4);
+console.log(`4값 위치 : ${arr.indexOf(4)}`);
+
+//배열 내장 객체 findIndex (indexof와 달리 값을 조작 할 수 있다.)
+const resultIndex = arr.findIndex((e) => {
+  return e === 5;
+});
+console.log(`4의 findIndex : ${arr.resultIndex(3)}`);
+
+//배열 내장 객체 find에 해당되는 객체를 찾아 리턴
+let arr5 = [{ name: "구길동" }, { name: "홍길동" }];
+
+const resultArray = arr5.find((e) => {
+  return e.name === "길동";
+});
+console.log(resultArray);
+
+//배열 내장 객체 filter에 해당되는 객체를 찾아 리턴
+let arr6 = [
+  { name: "구길동", hobby: "테니스" },
+  { name: "저길동", hobby: "테니스" },
+  { name: "홍길동", hobby: "독서" },
+];
+
+const resultArray2 = arr6.filter((e) => {
+  return e.hobby === "테니스";
+});
+console.log(resultArray2);
+
+//배열 내장 객체 map
+const resultArray3 = arr6.map((e) => {
+  return e.name;
+});
+console.log(resultArray3);
