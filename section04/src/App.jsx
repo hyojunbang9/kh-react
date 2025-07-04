b5975abff6c5e72beac4d1432e18ac5242e45f7f
hyojunbang9 <qkdgywns1004@gmail.com>
2025-07-04 15:41:04 +0900

    <react> useState

--- section04/src/App.jsx
+++ section04/src/App.jsx
@@ -1,34 +1,10 @@
 import { useState } from "react";
-import Bulb from "./compnents/Bulb";
-import BulbButton from "./compnents/BulbButton";
-import Count from "./compnents/Count";
-import CountButton from "./compnents/CountButton";
-
-//Bulb component(전구)
+import Register from "./compnents/register";
 
 export default function App() {
-  //상태 값
-  const [light, setLight] = useState("OFF");
-  //이벤트 핸들러
-  const lightClickBtn = () => {
-    setLight(light === "ON" ? "OFF" : "ON");
-  };
-
-  //상태 값
-  const [count, setCount] = useState(0);
-  //이벤ß트 핸들러
-  const countClickBtn = () => {
-    setCount(count + 1);
-  };
   return (
     <>
-      <div>
-        {/* Bulb 컴포넌트에 매생 상태값 light 전달 객체 생성 */}
-        <Bulb light={light} />
-        <BulbButton light={light} lightClickBtn={lightClickBtn} />
-        <Count count={count} />
-        <CountButton countClickBtn={countClickBtn} />
-      </div>
+      <Register />
     </>
   );
 }
--- section04/src/compnents/Register.jsx
+++ section04/src/compnents/Register.jsx
@@ -1,7 +1,56 @@
-function Register(props) {
+import { useState } from "react";
+
+function Register() {
+  //값을 입력해서 화면에 보여야 하는 곳은 useState
+  //값이 계속 유지 돼야 하는 곳도 useState
+  //값이 변동 될 경우 화면이 랜더링 돼야 하는 곳도 useState
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
+
+  const onChangeInput = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
   return (
     <div>
-      <h1>회원가입폼/</h1>
+      <div>
+        <input
+          value={input.name}
+          name={"name"}
+          onChange={onChangeInput}
+          placeholder={"이름"}
+        />
+      </div>
+
+      <div>
+        <input
+          value={input.birth}
+          name={"birth"}
+          onChange={onChangeInput}
+          type="date"
+        />
+      </div>
+      <div>
+        <select value={input.country} name="country" onChange={onChangeInput}>
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
+        </select>
+        {input.country}
+      </div>
+      <div>
+        <textarea
+          value={input.bio}
+          name="bio"
+          onChange={onChangeInput}
+          placeholder="어떤 인생을 살아오셨나요?"
+        />
+      </div>
     </div>
   );
 }
