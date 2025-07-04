4338c6e6d860ff127587310b657a093f1821edd5
hyojunbang9 <qkdgywns1004@gmail.com>
2025-07-04 16:21:22 +0900

    <react> useRef

--- section04/src/compnents/Register.jsx
+++ section04/src/compnents/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function Register() {
   //값을 입력해서 화면에 보여야 하는 곳은 useState
@@ -10,15 +10,32 @@ function Register() {
     country: "",
     bio: "",
   });
+  //상태 값 => 값을 잃어버리지 않는다.(리랜더링 xx), 객체 참조
+  const countRef = useRef(0); // const countRef = {current:0}
+  const nameRef = useRef(); // const countRef = {current: undefined}
 
+  //input 변경 이벤트 핸들러
   const onChangeInput = (e) => {
+    countRef.current++;
     setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  //submit 클릭 이벤트 핸들러
+  const onSubmit = (e) => {
+    if (nameRef.current.value === "") {
+      alert("이름을 입력해주세요.");
+      nameRef.current.focus();
+    } else {
+      alert("자료 전송 완료");
+    }
   };
 
   return (
     <div>
+      <h3>이벤트 발생 횟수{countRef.current}</h3>
       <div>
         <input
+          ref={nameRef}
           value={input.name}
           name={"name"}
           onChange={onChangeInput}
@@ -50,6 +67,10 @@ function Register() {
           onChange={onChangeInput}
           placeholder="어떤 인생을 살아오셨나요?"
         />
+      </div>
+
+      <div>
+        <button onClick={onSubmit}>서버로 전송</button>
       </div>
     </div>
   );
